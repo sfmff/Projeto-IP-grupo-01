890acb71504e1bc7307fad6be16b3689ed9dd257
Fernando Corrêa Gambôa Pereira dos Santos <fcgps>
2026-06-22 10:44:58 -0300

    Criação das condicionais para efeito das coletáveis

--- códigos/jogador.py
+++ códigos/jogador.py
@@ -6,11 +6,19 @@ pygame.init()
 class jogador():
 
     # Atributos do jogador (coordenadas iniciais e velocidade)
-    def __init__(self, x, y, largura_mapa):
+    def __init__(self, x, y, largura_mapa, power_up = False, slow_down = False):
         self.x = x
         self.y = y
         self.velocidade = 5
         self.largura_mapa = largura_mapa
+        self.power_up = power_up
+        self.slow_down = slow_down
+        # se o power_up for True, a velocidade aumenta em 1 unidade
+        if power_up:
+            self.velocidade += 1
+        # se o slow_down for True, a velocidade diminui em 1 unidade
+        if slow_down:
+            self.velocidade -= 1
 
 
     # Método (função) que controla o movimento do jogador (objeto)
