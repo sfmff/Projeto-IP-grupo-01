8ebf464bc3a374839c5fce5db29735203b06b65a
Fernando Corrêa Gambôa Pereira dos Santos <fcgps@cin.ufpe.br>
2026-06-25 16:24:14 -0300

    Criei um método que atualiza os atributos de acordo com a colisão

--- códigos/jogador.py
+++ códigos/jogador.py
@@ -5,8 +5,9 @@ pygame.init()
 # criação da classe do jogador (que será usada para criar o objeto "jogador" no main)
 class jogador():
 
+
     # Atributos do jogador (coordenadas iniciais e velocidade)
-    def __init__(self, x, y, largura_mapa, bateu_no_obstaculo = False,):
+    def __init__(self, x, y, largura_mapa,):
 
         self.image = pygame.image.load("assets/cenário e jogadores/neymar.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (120, 180))
@@ -17,21 +18,12 @@ class jogador():
         self.rect.y = y
         self.velocidade = 10
         self.largura_mapa = largura_mapa
-        self.bateu_no_obstaculo = bateu_no_obstaculo
         self.vidas = 5
 
         #coletáveis
         self.bolas_de_ouro = 0
         self.gatorade = 0
         self.caneleira = 0
-
-        # se o power_up for True, a velocidade aumenta em 1 unidade
-        if self.bolas_de_ouro >= 1:
-            self.velocidade += self.bolas_de_ouro
-
-        # se o bateu_no_obstaculo for True
-        if bateu_no_obstaculo:
-            self.vidas -= 1
 
 
     # Método (função) que controla o movimento do jogador (objeto)
@@ -45,3 +37,16 @@ class jogador():
         if teclas[pygame.K_RIGHT] and self.rect.right < self.largura_mapa:
             self.rect.x += self.velocidade
         self.hitbox.center = self.rect.center
+
+
+    # Método (função) que recebe o evento (colisão) e atualiza os atributos do jogador
+    def atualizaçao(self, evento):
+        if evento == "dano":
+            self.vidas -= 1
+        if evento == "bola_de_ouro":
+            self.bolas_de_ouro += 1
+            self.velocidade += 1
+        if evento == "gatorade":
+            self.gatorade += 1
+        if evento == "caneleira":
+            self.caneleira += 1
