6811b93c4149a4ac60ac5658f2023d2389c84a9a
Daniel Monteiro <dcmonteiro07@gmail.com>
2026-06-25 13:31:16 -0300

    Ajuste com as imagens corretas dos coletaveis, que estavam nos assets

--- códigos/coletaveis.py
+++ códigos/coletaveis.py
@@ -9,38 +9,41 @@ class Coletavel(pygame.sprite.Sprite):
         self.altura_tela = altura_tela
         self.tipo = tipo
         
-        # colocar imagens
-        self.image = pygame.Surface((40, 40))
-        
-        # funcoes
         if self.tipo == 'bola_ouro':
-            self.image.fill((255, 215, 0))
+            self.image = pygame.image.load('assets/imagens_jogo/bola_de_ouro.png').convert_alpha()
+            self.image = pygame.transform.scale(self.image, (40, 40)) 
+            
             self.pontos_ganhos = 500 
             self.multiplicador_bonus = 2.0 
             
         elif self.tipo == 'isotonico':
-            self.image.fill((0, 191, 255)) 
+            self.image = pygame.image.load('assets/imagens_jogo/isotonico.png').convert_alpha()
+            self.image = pygame.transform.scale(self.image, (40, 40)) 
+            
             self.tempo_duracao = 5000 
             self.da_invencibilidade = True
             
         elif self.tipo == 'caneleira':
-            self.image.fill((169, 169, 169)) 
+            self.image = pygame.image.load('assets/imagens_jogo/caneleira_aco.png').convert_alpha()
+            self.image = pygame.transform.scale(self.image, (40, 40)) 
+            
             self.concede_escudo = True
             
+        # hitbox e posicionamento na tela
         self.rect = self.image.get_rect()
         self.hitbox = self.rect.inflate(-10, -10)
         
         self.rect.y = -100
         self.rect.x = random.randint(0, self.largura_tela - self.rect.width)
         
-        # velocidade descida
         self.velocidade_y = random.randint(4, 7)
 
     def update(self):
         self.rect.y += self.velocidade_y
         
+        # ajustar hitbox
         self.hitbox.center = self.rect.center
         
-        # se sair da tela some
+        # se sair da tela é excluido
         if self.rect.top > self.altura_tela:
             self.kill()
